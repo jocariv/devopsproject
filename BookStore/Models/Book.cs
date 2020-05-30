946cad56139c5b3eeba610e467dff6ee3f407667
jocariv <jcrival@outlook.com>
2020-05-30 09:43:04 -0500

    Importing proyect

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreWithData.Models
+{
+    public class Book
+    {
+        internal List<string> Authors;
+
+        public int Id { get; set; }
+
+        [Display(Name = "Book Title")]
+        [Required]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Publish Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; }
+    }
+}
